7121a9951fe870c5af66f7f9feceb0631f794944
Guite <info@guite.de>
2010-07-09 11:57:55 +0000

    added MWE2 version of MWE workflows (not used/migrated yet)

---
+++ plugins/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/workflow/components/Logger.java
@@ -0,0 +1,26 @@
+package org.zikula.modulestudio.generator.workflow.components;
+
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent;
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+
+public class Logger implements IWorkflowComponent {
+	private String message = "Hello World!";
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public void invoke(IWorkflowContext ctx) {
+		System.out.println(getMessage());
+	}
+
+	@Override
+	public void postInvoke() {}
+
+	@Override
+	public void preInvoke() {}
+}
