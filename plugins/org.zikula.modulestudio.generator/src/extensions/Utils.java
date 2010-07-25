fedcd3b0e0a8b1c7da9afa52e7b2063cffcb2ee9
Guite <info@guite.de>
2010-07-25 19:09:00 +0000

    further work on different generator runs (mwe vs mwe2, inside Eclipse vs inside MOST vs. inside exported MOST)

--- plugins/org.zikula.modulestudio.generator/src/extensions/Utils.java
+++ plugins/org.zikula.modulestudio.generator/src/extensions/Utils.java
@@ -10,113 +10,121 @@ import org.eclipse.core.runtime.IProgressMonitor;
 
 public class Utils {
 
-	private static Boolean isInDebugMode = false;
+    private static Boolean isInDebugMode = false;
 
-	/**
-	 * @params    String    given input string
-	 * @return              string formatted for source code usage
-	 */
-	public static String applicationName(String string) {
-		if (isInDebugMode) {
-			System.out.println("applicationName (" + string + ")");
-		}
-		return replaceSpecialChars(string);
-	}
+    /**
+     * @params String given input string
+     * @return string formatted for source code usage
+     */
+    public static String applicationName(String string) {
+        if (isInDebugMode) {
+            System.out.println("applicationName (" + string + ")");
+        }
+        return replaceSpecialChars(string);
+    }
 
-	/**
-	 * @params    String    given input string
-	 * @return              string formatted for database usage
-	 */
-	public static String dbName(String string) {
-		if (isInDebugMode) {
-			System.out.println("dbName (" + string + ")");
-		}
-		return replaceSpecialChars(string).toLowerCase();
-	}
+    /**
+     * @params String given input string
+     * @return string formatted for database usage
+     */
+    public static String dbName(String string) {
+        if (isInDebugMode) {
+            System.out.println("dbName (" + string + ")");
+        }
+        return replaceSpecialChars(string).toLowerCase();
+    }
 
-	/**
-	 * @params    String    given input string
-	 * @return              string formatted (e.g. federalStateName becomes federal state names)
-	 */
-	public static String formattedName(String string) {
-		if (isInDebugMode) {
-			System.out.println("formattedName (" + string + ")");
-		}
-		String result = "";
-		String helpString = replaceSpecialChars(string);
+    /**
+     * @params String given input string
+     * @return string formatted (e.g. federalStateName becomes federal state
+     *         names)
+     */
+    public static String formattedName(String string) {
+        if (isInDebugMode) {
+            System.out.println("formattedName (" + string + ")");
+        }
+        String result = "";
+        String helpString = replaceSpecialChars(string);
 
-		char[] helpChars = helpString.toCharArray();
+        char[] helpChars = helpString.toCharArray();
 
-		for (char c : helpChars) {
-			String sc = String.valueOf(c);
-			if (sc.matches("[A-Z]")) result += " ";
-			result += sc.toLowerCase();
-		}
+        for (char c : helpChars) {
+            String sc = String.valueOf(c);
+            if (sc.matches("[A-Z]")) {
+                result += " ";
+            }
+            result += sc.toLowerCase();
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * @params    String    given input string
-	 * @return              string formatted (e.g. federalStateName becomes Federal state names)
-	 */
-	public static String formattedNameCapitalized(String string) {
-		if (isInDebugMode) {
-			System.out.println("formattedNameCapitalized (" + string + ")");
-		}
-		String result = "";
-		String helpString = replaceSpecialChars(string);
+    /**
+     * @params String given input string
+     * @return string formatted (e.g. federalStateName becomes Federal state
+     *         names)
+     */
+    public static String formattedNameCapitalized(String string) {
+        if (isInDebugMode) {
+            System.out.println("formattedNameCapitalized (" + string + ")");
+        }
+        String result = "";
+        String helpString = replaceSpecialChars(string);
 
-		char[] helpChars = helpString.toCharArray();
+        char[] helpChars = helpString.toCharArray();
 
-		int i = 0;
-		for (char c : helpChars) {
-			String sc = String.valueOf(c);
-			if (sc.matches("[A-Z]")) result += " ";
-			if (i == 0) {
-				result += sc.toUpperCase();				
-			}
-			else {
-				result += sc.toLowerCase();				
-			}
-			i++;
-		}
+        int i = 0;
+        for (char c : helpChars) {
+            String sc = String.valueOf(c);
+            if (sc.matches("[A-Z]")) {
+                result += " ";
+            }
+            if (i == 0) {
+                result += sc.toUpperCase();
+            }
+            else {
+                result += sc.toLowerCase();
+            }
+            i++;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * @params    String    given input string
-	 * @return              string without special characters
-	 */
-	public static String replaceSpecialChars(String string) {
-		if (isInDebugMode) {
-			System.out.println("replaceSpecialChars (" + string + ")");
-		}
-		return string.replace("Ä", "Ae").replace("ä", "ae")
-					 .replace("Ö", "Oe").replace("ö", "oe")
-					 .replace("Ü", "Ue").replace("ü", "ue")
-					 .replace("ß", "ss").replaceAll("[\\W]", "");
-	}
+    /**
+     * @params String given input string
+     * @return string without special characters
+     */
+    public static String replaceSpecialChars(String string) {
+        if (isInDebugMode) {
+            System.out.println("replaceSpecialChars (" + string + ")");
+        }
+        return string.replace("Ä", "Ae").replace("ä", "ae").replace("Ö", "Oe")
+                .replace("ö", "oe").replace("Ü", "Ue").replace("ü", "ue")
+                .replace("ß", "ss").replaceAll("[\\W]", "");
+    }
 
+    /**
+     * @return the current timestamp to mark the generation time
+     */
+    public static String timestamp() {
+        return String.valueOf(new Date(System.currentTimeMillis()));
+    }
 
+    /**
+     * @param obj
+     * @return
+     */
+    public static Boolean isProgressMonitor(Object obj) {
+        return (obj instanceof IProgressMonitor);
+    }
 
-
-	/**
-	 * @return        the current timestamp to mark the generation time
-	 */
-	public static String timestamp() {
-		return String.valueOf(new Date(System.currentTimeMillis()));
-	}
-
-
-
-	/**
-	 * @params    IProgressMonitor  progress monitor instance
-	 * @params    String    given input string
-	 */
-	public static String subTask(IProgressMonitor pm, String newTitle) {
-		pm.subTask(newTitle);
-		return "";
-	}
+    /**
+     * @params IProgressMonitor progress monitor instance
+     * @params String given input string
+     */
+    public static String subTask(IProgressMonitor pm, String newTitle) {
+        pm.subTask(newTitle);
+        return "";
+    }
 }
--- plugins/org.zikula.modulestudio.generator/src/transformation/TrafoUtils.java
+++ plugins/org.zikula.modulestudio.generator/src/transformation/TrafoUtils.java
@@ -1,11 +1,9 @@
 package transformation;
 
-import java.util.Iterator;
-
+import de.guite.modulestudio.metamodel.modulestudio.Entity;
 import de.guite.modulestudio.metamodel.modulestudio.IntegerField;
 import de.guite.modulestudio.metamodel.modulestudio.ModulestudioFactory;
 import de.guite.modulestudio.metamodel.modulestudio.Relationship;
-import de.guite.modulestudio.metamodel.modulestudio.Entity;
 import de.guite.modulestudio.metamodel.modulestudio.impl.ModulestudioFactoryImpl;
 import extensions.Utils;
 
@@ -15,54 +13,50 @@ import extensions.Utils;
 
 public class TrafoUtils {
 
-	/**
-	 * add a primary key to a table
-	 * 
-	 * @params    Entity         given Entity instance
-	 * @return                   flag if insertion was sucessful
-	 */
-	public static boolean addPrimaryKey(Entity entity) {
-		try {
-			entity.getFields().add(0, createIDColumn(entity.getName(), true));
-		}
-		catch (Exception e) {
-			return false;
-		}
-		finally {
-			// nothing to do here (yet)
-		}
-		return true;
-	}
+    /**
+     * add a primary key to a table
+     * 
+     * @params Entity given Entity instance
+     * @return flag if insertion was sucessful
+     */
+    public static boolean addPrimaryKey(Entity entity) {
+        try {
+            entity.getFields().add(0, createIDColumn(entity.getName(), true));
+        } catch (Exception e) {
+            return false;
+        } finally {
+            // nothing to do here (yet)
+        }
+        return true;
+    }
 
-	/**
-	 * add a relation id fields to a table
-	 * 
-	 * @params    Entity         given Entity instance
-	 * @return                   flag if process was sucessful
-	 */
-	public static boolean addRelationFields(Entity entity) {
-		try {
-			for (Iterator relIter = entity.getIncoming().iterator(); relIter.hasNext();) {
-				Relationship rel = (Relationship) relIter.next();
-				entity.getFields().add(createIDColumn(rel.getSource().getName(), false));
-			}
-		}
-		catch (Exception e) {
-			return false;
-		}
-		finally {
-			// nothing to do here (yet)
-		}
-		return true;
-	}
+    /**
+     * add a relation id fields to a table
+     * 
+     * @params Entity given Entity instance
+     * @return flag if process was sucessful
+     */
+    public static boolean addRelationFields(Entity entity) {
+        try {
+            for (Object element : entity.getIncoming()) {
+                Relationship rel = (Relationship) element;
+                entity.getFields().add(
+                        createIDColumn(rel.getSource().getName(), false));
+            }
+        } catch (Exception e) {
+            return false;
+        } finally {
+            // nothing to do here (yet)
+        }
+        return true;
+    }
 
-	private static IntegerField createIDColumn(String colName, Boolean isPrimary) {
-		ModulestudioFactory factory = new ModulestudioFactoryImpl();
-		IntegerField idField = factory.createIntegerField();
-		idField.setName(Utils.dbName(colName + "id"));
-		idField.setPrimaryKey(isPrimary);
-		idField.setLength(11);
-		return idField;
-	}
-
+    private static IntegerField createIDColumn(String colName, Boolean isPrimary) {
+        ModulestudioFactory factory = new ModulestudioFactoryImpl();
+        IntegerField idField = factory.createIntegerField();
+        idField.setName(Utils.dbName(colName + "id"));
+        idField.setPrimaryKey(isPrimary);
+        idField.setLength(11);
+        return idField;
+    }
 }
