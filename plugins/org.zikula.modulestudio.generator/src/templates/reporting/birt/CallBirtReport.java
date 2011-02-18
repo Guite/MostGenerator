dcdbd1d4f1fbc278e592a25529f723c83a8b9ebc
gfr <gabriel@freinbichler.net>
2011-02-18 14:50:09 +0000

    First Report for FPs and BIRT Runtime

--- plugins/org.zikula.modulestudio.generator/src/templates/reporting/birt/CallBirtReport.java
+++ plugins/org.zikula.modulestudio.generator/src/templates/reporting/birt/CallBirtReport.java
@@ -9,22 +9,43 @@ import org.eclipse.birt.report.engine.api.IReportRunnable;
 import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
 
 public class CallBirtReport {
+    public static void main() {
+        test();
+    }
 
     public static void test() {
+
+        IReportEngine engine = null;
+        EngineConfig config = null;
+
         try {
             // http://wiki.eclipse.org/RCP_Example_%28BIRT%29_2.1
             // http://wiki.eclipse.org/Simple_Execute_%28BIRT%29_2.1
-            final EngineConfig config = new EngineConfig();
+            config = new EngineConfig();
 
+            // use this to set the resource path
+            // final Bundle bundle =
+            // org.eclipse.core.runtime.Platform.getBundle("templates.reporting.birt");
+            // System.out.println(bundle);
+            // final URL url = FileLocator.find(bundle, new
+            // Path("/ReportEngine"), null);
+
+            // final String myresourcepath =
+            // FileLocator.toFileURL(url).getPath();
+            // config.setResourcePath(myresourcepath);
+
+            config.setBIRTHome("/home/gabriel/workspace/Maturaprojekt/org.zikula.modulestudio.generator/src/templates/reporting/birt/ReportEngine");
+
+            Platform.startup(config);
             final IReportEngineFactory factory = (IReportEngineFactory) Platform
                     .createFactoryObject(IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
-            final IReportEngine engine = factory.createReportEngine(config);
+            engine = factory.createReportEngine(config);
 
             IReportRunnable design = null;
             // Open the report design
 
             design = engine
-                    .openReportDesign("/home/gabriel/workspace/Maturaprojekt/org.zikula.modulestudio.generator/src/templates/reporting/birt/testreport.rtpdesign");
+                    .openReportDesign("/home/gabriel/workspace/Maturaprojekt/org.zikula.modulestudio.generator/src/templates/reporting/birt/testreport.rptdesign");
             final IRunAndRenderTask task = engine
                     .createRunAndRenderTask(design);
             // task.setParameterValue("Top Count", (new Integer(5)));
