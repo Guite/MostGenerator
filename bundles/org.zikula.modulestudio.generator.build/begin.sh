af770e84677c62a7925db1ba163297835798e75c
Guite <info@guite.de>
2011-02-24 00:40:19 +0100

    build-related changes

--- bundles/org.zikula.modulestudio.generator.build/begin.sh
+++ bundles/org.zikula.modulestudio.generator.build/begin.sh
@@ -1,5 +1,3 @@
-echo "Linking project git repository"
-ln -sf /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm/ $WORKSPACE/refProduct
 echo "Removing old meta data to ensure clean new p2 site"
 rm -Rf $WORKSPACE/.metadata
 rm -Rf $WORKSPACE/features
