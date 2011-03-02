82aa4850e0e21f2956defd03addf951d3df79a1a
Guite <info@guite.de>
2011-03-02 13:48:11 +0100

    build-related changes

--- bundles/org.zikula.modulestudio.generator.build/begin.sh
+++ bundles/org.zikula.modulestudio.generator.build/begin.sh
@@ -5,3 +5,5 @@ rm -Rf $WORKSPACE/plugins
 rm -Rf $WORKSPACE/output
 rm -Rf $WORKSPACE/temp
 echo "Preparation completed"
+echo "Linking project git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
