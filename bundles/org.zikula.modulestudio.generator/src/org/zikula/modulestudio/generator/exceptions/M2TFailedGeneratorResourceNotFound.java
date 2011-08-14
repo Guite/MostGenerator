47f356c1e0ed63376ce79b2084453fe190af22f3
Guite <info@guite.de>
2011-08-14 02:00:46 +0200

    removing static member vars

--- bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/exceptions/M2TFailedGeneratorResourceNotFound.java
+++ bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/exceptions/M2TFailedGeneratorResourceNotFound.java
@@ -1,10 +1,6 @@
 package org.zikula.modulestudio.generator.exceptions;
 
 /** TODO: javadocs needed for class, members and methods */
+@SuppressWarnings("serial")
 public class M2TFailedGeneratorResourceNotFound extends ExceptionBase {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
 }
