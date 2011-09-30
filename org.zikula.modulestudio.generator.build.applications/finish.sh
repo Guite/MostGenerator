ddf8b0eb9ccfeb7577df3b6e7a00c32e9ed83b6e
Guite <info@guite.de>
2011-09-30 20:33:48 +0200

    build-related changes

--- org.zikula.modulestudio.generator.build.applications/finish.sh
+++ org.zikula.modulestudio.generator.build.applications/finish.sh
@@ -1,4 +1,4 @@
-cat $WORKSPACE/scm/bundles/org.zikula.modulestudio.generator.build.applications/build.log
+cat $WORKSPACE/scm/org.zikula.modulestudio.generator.build.applications/build.log
 
 echo "Removing working directory."
 rm -rf tempWorkingDir
