0f6241f2ffec4b477b69d3ea233b4bbb40e93f2b
Guite <info@guite.de>
2012-11-04 16:33:47 +0100

    basic migration of reporting cartridge, testing will be done with alpha release

--- org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/reporting/reports/reportingHelpers.js
+++ org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/reporting/reports/reportingHelpers.js
@@ -8,17 +8,17 @@ function countRows(row, attribute)
 {
     var sum;
     sum = countElements(row);
-    if(attribute["geographical"])
-        sum +=2;
-    if(attribute["sluggable"])
-        sum +=1;
-    if(attribute["standardFields"])
-        sum +=5;
-    if(attribute["versionable"])
-        sum +=1;
-        
-    return sum;
+    if (attribute['geographical']) {
+        sum += 2;
+    }
+    if (attribute['standardFields']) {
+        sum += 5;
+    }
+    if (attribute['tree']) {
+        sum += 6;
+    }
 
+    return sum;
 }
 
 function countElements(row)
@@ -26,43 +26,39 @@ function countElements(row)
     if (row.length == 0) {
         return 0;
     }
+
     return row.split("\n").length;
-
 }
 
-function countComplexityNiedrig() {
-    return summe;
-}
-
-function calculateComplexityAnwenderdaten(cols, relations)
+function calculateComplexityApplicationData(columns, relations)
 {
-    if (relations < 2 && cols > 15 || relations < 3 && cols > 4 && cols < 16 || relations >= 3 && cols < 5){
+    if (relations < 2 && columns > 15 || relations < 3 && columns > 4 && columns < 16 || relations >= 3 && columns < 5) {
         return 1;
-    }else if (relations < 3 && cols > 15 || relations >= 3 && cols > 4) {
+    } else if (relations < 3 && columns > 15 || relations >= 3 && columns > 4) {
         return 2;
-    }else{
+    } else{
         return 0;
-    }    
+    }
 }
 
-function calculateComplexityEingaben(fields, relations)
+function calculateComplexityInputs(fields, relations)
 {
-    if (relations < 2 && fields > 15 || relations < 3 && fields > 4 && fields < 16 || relations >= 3 && fields < 5){
+    if (relations < 2 && fields > 15 || relations < 3 && fields > 4 && fields < 16 || relations >= 3 && fields < 5) {
         return 1;
-    }else if (relations < 3 && fields > 15 || relations >= 3 && fields > 4) {
+    } else if (relations < 3 && fields > 15 || relations >= 3 && fields > 4) {
         return 2;
-    }else{
+    } else {
         return 0;
-    }    
+    }
 }
 
-function calculateComplexityAusgabe(fields, relations)
+function calculateComplexityOutputs(fields, relations)
 {    
-    if (relations < 2 && fields > 50 || relations < 6 && fields > 19 && fields < 51 || relations > 5 && fields < 20){
+    if (relations < 2 && fields > 50 || relations < 6 && fields > 19 && fields < 51 || relations > 5 && fields < 20) {
         return 1;
-    }else if (relations < 6 && fields > 50 || relations > 5 && fields > 19){
+    } else if (relations < 6 && fields > 50 || relations > 5 && fields > 19) {
         return 2;
-    }else{
+    } else {
         return 0;
     }
 }
@@ -70,8 +66,7 @@ function calculateComplexityAusgabe(fields, relations)
 function sumUp(source)
 {
     sum = 0;
-    for(i = 0;i < 3; i++)
-    {
+    for (i = 0;i < 3; i++) {
         sum += parseInt(source[i]);
     }
     return sum;
