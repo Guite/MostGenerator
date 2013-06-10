845f0d92b42482ff8353ada0b20f7359a130e86c
Guite <info@guite.de>
2013-06-10 19:14:24 +0200

    overhauled build targets

--- org.zikula.modulestudio.generator.build/begin.sh
+++ org.zikula.modulestudio.generator.build/begin.sh
@@ -2,3 +2,5 @@
 WORKSPACE=$1
 echo "Linking project git repository"
 ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
+
+mkdir -p $WORKSPACE/output/test_results/coverage/
