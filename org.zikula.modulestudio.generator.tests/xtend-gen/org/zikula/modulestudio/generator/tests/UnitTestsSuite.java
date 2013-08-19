9f12e1167d2cf8373d8d7eb4737bb749b7f739d1
Guite <info@guite.de>
2013-08-19 18:58:15 +0200

    outsource importer into mostdsl feature to reduce UI deps in the generator

--- org.zikula.modulestudio.generator.tests/xtend-gen/org/zikula/modulestudio/generator/tests/UnitTestsSuite.java
+++ org.zikula.modulestudio.generator.tests/xtend-gen/org/zikula/modulestudio/generator/tests/UnitTestsSuite.java
@@ -7,10 +7,9 @@ import org.junit.runner.RunWith;
 import org.zikula.modulestudio.generator.tests.ApplicationPartsSuite;
 import org.zikula.modulestudio.generator.tests.CartridgesSuite;
 import org.zikula.modulestudio.generator.tests.GeneralExtensionsSuite;
-import org.zikula.modulestudio.generator.tests.XMLImporterSuite;
 
 @Named("Unit tests")
-@Contains({ ApplicationPartsSuite.class, GeneralExtensionsSuite.class, CartridgesSuite.class, XMLImporterSuite.class })
+@Contains({ ApplicationPartsSuite.class, GeneralExtensionsSuite.class, CartridgesSuite.class })
 @RunWith(ExampleGroupRunner.class)
 @SuppressWarnings("all")
 public class UnitTestsSuite {
