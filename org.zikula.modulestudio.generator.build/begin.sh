a6103082f745263b0732ca45b9849ce8ec6017a7
Michael Eckes <michael.eckes@gmail.com>
2013-09-03 16:02:25 +0200

    fixed typo

--- org.zikula.modulestudio.generator.build/begin.sh
+++ org.zikula.modulestudio.generator.build/begin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 WORKSPACE=$1
-GIT_REVISON=$2
+GIT_REVISION=$2
 echo "Linking project git repository"
 ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
 
