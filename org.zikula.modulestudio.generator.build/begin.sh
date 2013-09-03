11b99458d4359421714a2f9b8981557964aae249
Michael Eckes <michael.eckes@gmail.com>
2013-09-03 13:56:04 +0200

    Added git revision hash to bundle

--- org.zikula.modulestudio.generator.build/begin.sh
+++ org.zikula.modulestudio.generator.build/begin.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 WORKSPACE=$1
+GIT_REVISON=$2
 echo "Linking project git repository"
 ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
 
 mkdir -p $WORKSPACE/output/test_results/coverage/
+
+echo "writing git revision to file"
+echo $GIT_REVISION > $WORKSPACE/scm/org.zikula.modulestudio.generator/src/gitrevision.txt
