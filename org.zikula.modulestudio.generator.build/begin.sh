27bc01b8ddd066e7490526c2929513dcb7ebc17d
Guite <info@guite.de>
2013-09-27 18:18:42 +0200

    corrected path

--- org.zikula.modulestudio.generator.build/begin.sh
+++ org.zikula.modulestudio.generator.build/begin.sh
@@ -5,7 +5,7 @@ echo "Linking project git repository"
 ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
 
 echo "Cleaning xtend-gen folder"
-rm -rf "$WORKSPACE/xtend-gen/*"
+rm -rf "$WORKSPACE/scm/org.zikula.modulestudio.generator/xtend-gen/*"
 
 mkdir -p $WORKSPACE/output/test_results/coverage/
 
