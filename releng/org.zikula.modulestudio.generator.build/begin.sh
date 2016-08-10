6bd5df7b6db9ed242e2a53db43be97f5d02820ea
Guite <info@guite.de>
2016-08-10 20:01:42 +0200

    removed obsolete file

--- releng/org.zikula.modulestudio.generator.build/begin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-WORKSPACE=$1
-GIT_REVISION=$2
-
-echo "Writing current git revision into file"
-echo $GIT_REVISION > $WORKSPACE/scm/bundles/org.zikula.modulestudio.generator/src/gitrevision.txt
