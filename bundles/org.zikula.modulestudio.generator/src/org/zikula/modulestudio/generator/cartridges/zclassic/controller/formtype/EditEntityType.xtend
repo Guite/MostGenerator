Guite/MostGenerator

commit 13391eab3db25dddbd14d73373eefd7392dc55a6

fixed idPrefix for initialising UI hook provider auto completion

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/zclassic/controller/javascript/EditFunctions.xtend
@@ -180,7 +180,7 @@ class EditFunctions {
         function «vendorAndName»InitRelationHandling(objectType, alias, idPrefix, includeEditing, inputType, createUrl) {
             «IF needsAutoCompletion»
                 if (inputType == 'autocomplete') {
-                    «vendorAndName»InitAutoCompletion(objectType, idPrefix, includeEditing);
+                    «vendorAndName»InitAutoCompletion(objectType, alias, includeEditing);
                 }
             «ENDIF»
             «IF needsInlineEditing»

commit 72b96c00c5ae3fc71297cd5b1255bc65b13b6f10

fixed broken auto completion caused by 13391eab3db25dddbd14d73373eefd7392dc55a6

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/zclassic/controller/formtype/EditEntityType.xtend
@@ -629,7 +629,7 @@ class EditEntityType {
             «val isExpanded = if (outgoing) expandedTarget else expandedSource»
             $builder->add('«aliasName.formatForCode»', '«formType(autoComplete)»Type', [
                 «IF autoComplete»
-                    «val uniqueNameForJs = getUniqueRelationNameForJs((if (outgoing) source else target), isManySide(outgoing), (if (!isManyToMany) outgoing else !outgoing), aliasName.formatForCodeCapital)»
+                    «val uniqueNameForJs = getUniqueRelationNameForJs((if (outgoing) source else target), aliasName.formatForCodeCapital)»
                     'object_type' => '«relatedEntity.name.formatForCode»',
                     «IF isManySide(outgoing)»
                         'by_reference' => false,
@@ -731,13 +731,6 @@ class EditEntityType {
     def private formType(JoinRelationship it, Boolean autoComplete) {
         if (autoComplete) '''«app.appNamespace»\Form\Type\Field\AutoCompletionRelation'''
         else '''Symfony\Bridge\Doctrine\Form\Type\Entity'''
-    }
-
-    def private isManyToMany(JoinRelationship it) {
-        switch it {
-            ManyToManyRelationship: true
-            default: false
-        }
     }
 
     def private addAdditionalNotificationRemarksField(Entity it) '''

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/zclassic/controller/javascript/AutoCompletion.xtend
@@ -177,8 +177,15 @@ class AutoCompletion {
         /**
          * Initialises auto completion for a relation field.
          */
-        function «vendorAndName»InitAutoCompletion(objectType, idPrefix, includeEditing) {
+        function «vendorAndName»InitAutoCompletion(objectType, alias, idPrefix, includeEditing) {
             var acOptions, acDataSet, acUrl«IF hasUiHooksProviders», isHookAttacher«ENDIF»;
+            «IF hasUiHooksProviders»
+
+                isHookAttacher = idPrefix.startsWith('hookAssignment');
+                if (isHookAttacher) {
+                    idPrefix = alias;
+                }
+            «ENDIF»
 
             // update identifier of hidden field for easier usage in JS
             jQuery('#' + idPrefix + 'Multiple').prev().attr('id', idPrefix);
@@ -196,10 +203,6 @@ class AutoCompletion {
             // clear values and ensure starting state
             «vendorAndName»ResetAutoCompletion(idPrefix);
 
-            «IF hasUiHooksProviders»
-
-                isHookAttacher = idPrefix.startsWith('hookAssignment');
-            «ENDIF»
             jQuery.each(«vendorAndName»InlineEditHandlers, function (key, editHandler) {
                 if (editHandler.prefix !== (idPrefix + 'SelectorDoNew') || editHandler.inputType !== 'autocomplete') {
                     return;

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/zclassic/controller/javascript/EditFunctions.xtend
@@ -180,7 +180,7 @@ class EditFunctions {
         function «vendorAndName»InitRelationHandling(objectType, alias, idPrefix, includeEditing, inputType, createUrl) {
             «IF needsAutoCompletion»
                 if (inputType == 'autocomplete') {
-                    «vendorAndName»InitAutoCompletion(objectType, alias, includeEditing);
+                    «vendorAndName»InitAutoCompletion(objectType, alias, idPrefix, includeEditing);
                 }
             «ENDIF»
             «IF needsInlineEditing»

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/cartridges/zclassic/view/formcomponents/Relations.xtend
@@ -88,8 +88,7 @@ class Relations {
 
         if (onlyInclude) {
             val relationAliasName = getRelationAliasName(useTarget).formatForCodeCapital
-            val incomingForUniqueRelationName = if (!isManyToMany) useTarget else !useTarget
-            val uniqueNameForJs = getUniqueRelationNameForJs(otherEntity, many, incomingForUniqueRelationName, relationAliasName)
+            val uniqueNameForJs = getUniqueRelationNameForJs(otherEntity, relationAliasName)
             return includeStatementForEditTemplate(templateName, ownEntity, otherEntity, useTarget, relationAliasName, uniqueNameForJs)
         }
 
@@ -252,8 +251,7 @@ class Relations {
         }
 
         val relationAliasName = getRelationAliasName(!incoming).formatForCodeCapital
-        val many = isManySide(useTarget)
-        val uniqueNameForJs = getUniqueRelationNameForJs(targetEntity, many, incoming, relationAliasName)
+        val uniqueNameForJs = getUniqueRelationNameForJs(targetEntity, relationAliasName)
         val linkEntity = if (targetEntity == target) source else target
         if (!insideLoader) '''
             «app.vendorAndName»EditHandler = {

bundles/org.zikula.modulestudio.generator/src/org/zikula/modulestudio/generator/extensions/ModelJoinExtensions.xtend
@@ -279,7 +279,7 @@ class ModelJoinExtensions {
      * Returns a unique name for a relationship used by JavaScript during editing entities with auto completion fields.
      * The name is concatenated from the edited entity as well as the relation alias name.
      */
-    def getUniqueRelationNameForJs(JoinRelationship it, DataObject targetEntity, Boolean many, Boolean incoming, String relationAliasName) {
+    def getUniqueRelationNameForJs(JoinRelationship it, DataObject targetEntity, String relationAliasName) {
         application.prefix
         + targetEntity.name.formatForCodeCapital
         + '_'