398cbe799620906227aa7f6d5f99a0b9a4e15b33
Guite <info@guite.de>
2019-12-09 07:32:23 +0100

    trigger modules regenerations after standalone generator has been deployed

---
+++ .github/scripts/regenerateModules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# list projects
+PROJECTS=(
+    "Guite/test-actions"
+    "Guite/Awards"
+    "zikula-modules/Content"
+    "zikula-modules/MultiHook"
+    "zikula-modules/Multisites"
+    "zikula-modules/News"
+)
+
+# loop through projects
+for PROJECT in "${PROJECTS[@]}"
+do
+    echo "Trigger ${PROJECT}"
+    curl POST -H "Authorization: token ${DISPATCH_TOKEN}" \
+              -H "Accept: application/vnd.github.everest-preview+json"  \
+              -H "Content-Type: application/json" \
+              "https://api.github.com/repos/${PROJECT}/dispatches" \
+              --data '{"event_type": "generator-updated"}' \
+              --silent
+done
